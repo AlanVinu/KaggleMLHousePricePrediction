809b16baf845c3feb0f9084c5907c68dcab70a29
AlanVinu <alanvinuabraham@gmail.com>
2018-09-26 13:44:46 +0530

    Corrected overfitting and underfitting. RandomForest in use.

--- Code.py
+++ Code.py
@@ -43,3 +43,35 @@ print(melbourne_model.predict(X.head()))
 from sklearn.metrics import mean_absolute_error
 predicted_home_prices = melbourne_model.predict(X)
 mean_absolute_error(y, predicted_home_prices)
+
+#Validation data to be taken to determine the model's accuracy
+from sklearn.model_selection import train_test_split
+train_X, val_X, train_y, val_y = train_test_split(X, y, random_state = 0)
+#Model definition
+melbourne_model = DecisionTreeRegressor()
+#fitting
+melbourne_model.fit(train_X, train_y)
+#get predicted prices on validation data
+val_predictions = melbourne_model.predict(val_X)
+print(mean_absolute_error(val_y, val_predictions))
+
+#finding the sweet spot between overfitting and underfitting
+def get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y) :
+    model = DecisionTreeRegressor(max_leaf_nodes = max_leaf_nodes, random_state = 0)
+    model.fit(train_X, train_y)
+    preds_val = model.predict(val_X)
+    mae = mean_absolute_error(val_y, preds_val)
+    return(mae)
+    
+#comparing mae with different values of max_leaf_nodes
+for max_leaf_nodes in [5, 50, 500, 5000] :
+    my_mae = get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y)
+    print("Max leaf nodes : %d \t\t Mean Absolute Error: %d" %(max_leaf_nodes, my_mae))
+    
+#Improving prediction using RandomForest
+from sklearn.ensemble import RandomForestRegressor
+
+forest_model = RandomForestRegressor(random_state = 1)
+forest_model.fit(train_X, train_y)
+melb_preds = forest_model.predict(val_X)
+print(mean_absolute_error(val_y, melb_preds))
